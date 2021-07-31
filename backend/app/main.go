856f4bfc45469124f4cd4cc31d1233a6228d1c6e
JoelD7 <joeld200013@gmail.com>
2021-07-31 16:31:20 -0400

    Load transactions data to DB

--- backend/app/main.go
+++ backend/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -15,12 +16,20 @@ import (
 )
 
 type Buyer struct {
-	Name         string        `json:"name,omitempty"`
-	Transactions []Transaction `json:"transactions,omitempty"`
+	BuyerId      string
+	Name         string
+	Transactions []Transaction
+	Type         string `json:"dgraph.type,omitempty"`
 }
 
 type Transaction struct {
-	Device string `json:"device,omitempty"`
+	TransactionId string
+	BuyerId       string
+	Ip            string
+	Device        string
+	Products      []string
+	Date          string
+	Type          string `json:"dgraph.type,omitempty"`
 }
 
 func main() {
@@ -29,34 +38,7 @@ func main() {
 	txn := dgraphClient.NewTxn()
 	defer txn.Discard(ctx)
 
-	const q = `{
-		buyer(func: has(name)) {
-			name
-			transactions
-		}
-	}
-`
-
-	parseTransactions()
-
-	// resp, err := txn.Query(context.Background(), q)
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// type Root struct {
-	// 	Buyers []Buyer `json:"buyer"`
-	// }
-
-	// var r Root
-	// err = json.Unmarshal(resp.Json, &r)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// out, _ := json.MarshalIndent(r, "", "\t")
-	// fmt.Printf("%s\n", out)
+	fetchTransactions(txn)
 
 }
 
@@ -70,7 +52,26 @@ func newClient() *dgo.Dgraph {
 	return dgo.NewDgraphClient(dc)
 }
 
-func parseTransactions() {
+func fetchTransactions(txn *dgo.Txn) {
+	jsonTransactions := parseTransactions()
+
+	mutation := &api.Mutation{
+		SetJson:   jsonTransactions,
+		CommitNow: true,
+	}
+
+	_, err := txn.Mutate(context.Background(), mutation)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("New transactions saved.")
+}
+
+func parseTransactions() []byte {
+	var transactions []Transaction
+
 	req, err := http.NewRequest("GET", "https://kqxty15mpg.execute-api.us-east-1.amazonaws.com/transactions", nil)
 
 	if err != nil {
@@ -90,10 +91,11 @@ func parseTransactions() {
 	defer resp.Body.Close()
 	body, _ := io.ReadAll(resp.Body)
 
-	transactions := strings.Split(string(body), "#")
+	rawTransactions := strings.Split(string(body), "#")
+	transactionsQty := len(rawTransactions)
 
-	for i := 0; i < len(transactions); i++ {
-		line := transactions[i]
+	for i := 0; i < transactionsQty; i++ {
+		line := rawTransactions[i]
 		size := len(line)
 
 		if size == 0 {
@@ -115,7 +117,33 @@ func parseTransactions() {
 		ip := line[21 : deviceIndex+21]
 		device := line[deviceIndex+21 : productIndex+21]
 		products := line[productIndex+21:]
+		products = products[1 : len(products)-3]
 
-		fmt.Printf("transactionId: %s, buyerId: %s, ip: %s, device: %s, products: %s\n", transactionId, buyerId, ip, device, products)
+		newTransaction := Transaction{
+			BuyerId:       buyerId,
+			TransactionId: transactionId,
+			Ip:            ip,
+			Device:        device,
+			Products:      strings.Split(products, ","),
+			Date:          "2020-08-17",
+			Type:          "Transaction",
+		}
+
+		transactions = append(transactions, newTransaction)
+
 	}
+
+	rawJsonTransactions, err := json.Marshal(transactions)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	/*
+		Replace all appearances of the unicode null character: \u0000 with an
+		empty string.
+	*/
+	jsonTransactions := strings.Replace(string(rawJsonTransactions), "\\u0000", "", -1)
+	return []byte(jsonTransactions)
+
 }
